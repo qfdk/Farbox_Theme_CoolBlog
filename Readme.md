e65d03fe43bb6155262ca05d06a4f042dd1f1c5f
qfdk <qfdk2010@gmial.com>
2015-01-07 21:41:14 +0100

    修复评论 以及联系方式

--- Readme.md
+++ Readme.md
@@ -1,5 +1,5 @@
 ##升级日志
-
+* 2015-01-07 修复了评论 bug
 * 2014-06-22 完成了大部分的操作, 修复了部分bug, 添加了404 页面
 * 修复了很多的逻辑bug
 * 2014-06-21 开始实行新的模板制作
@@ -14,12 +14,15 @@
 
 
 ##使用说明
-复制`template`文件夹到Frabox 里面的项目的根目录
+* 复制`template`文件夹到Frabox 里面的项目的根目录
 刷新网站即可,记住是默认模板.
 
+或者
+
+* 直接访问 [这里](http://blog.qfdk.me/template) 来直接复制模板（推荐）。
 ##写在最后
 * 效果参照本人blog <http://qfdk.me>.
-* Email：<qfdk2010@gmial.com>
+* Email：<root@qfdk.me>
 * 完成于22/06/2014
 * Clermont-Ferrand
 * by 清风刀客
--- template/js/myApp.js
+++ template/js/myApp.js
@@ -1,13 +1,13 @@
 function send(){
-$.getJSON('http://qfdk.me/md5.php', {md5:$("#md5").val()}, function(json, textStatus) {
-    /*optional stuff to do after success */
-    console.log(textStatus);
-    if(textStatus=='success')
-    {
-      if(json.result!=null)
-      	$("#res").html("<p>"+json.result+"</p>");
-      else
-      	$("#res").html("<p>没有结果呢.</p>");
-    }
-});
+// $.getJSON('http://qfdk.me/md5.php', {md5:$("#md5").val()}, function(json, textStatus) {
+//     /*optional stuff to do after success */
+//     if(textStatus=='success')
+//     {
+//       if(json.result!=null)
+//       	$("#res").html("<p>"+json.result+"</p>");
+//       else
+//       	$("#res").html("<p>没有结果呢.</p>");
+//     }
+// });
+$("#res").html("<p>暂停查询...</p>");
 }
